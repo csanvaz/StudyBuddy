52414d538146bbb99dc79879330316e659e844dd
Seth Strzechowski <sstrz2@uic.edu>
2024-11-14 22:51:18 -0600

    trying something

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const client = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 const upload = multer({ dest: 'uploads/' });
 const cors = require('cors');
 app.use(cors({
-  origin: 'https://main.d1v5rs7h6klasx.amplifyapp.com/',
+  origin: 'https://main.d1v5rs7h6klasx.amplifyapp.com',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
 }));
