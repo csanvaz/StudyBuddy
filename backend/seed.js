79c787c217aced4c784f7830008f58518d2a31ab
Seth Strzechowski <sstrz2@uic.edu>
2024-12-03 22:08:40 -0600

    fix seed problem

--- backend/seed.js
+++ backend/seed.js
@@ -20,7 +20,7 @@ async function createTables() {
                 username VARCHAR(50) UNIQUE NOT NULL,
                 email VARCHAR(100) UNIQUE NOT NULL,
                 password VARCHAR(255) NOT NULL,
-                avatar VARCHAR(255)
+                avatar VARCHAR(255),
                 last_login TIMESTAMP DEFAULT NOW()
             );
         `);
