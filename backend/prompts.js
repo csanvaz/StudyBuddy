b38a598e39c6a466ec625d16afa9c32bd274b65c
Carlos Moreles <cmore20@uic.edu>
2024-12-05 14:11:18 -0600

    prompts fix

--- backend/prompts.js
+++ backend/prompts.js
@@ -2,9 +2,10 @@ const flashCardPrompt = `identity: You are an expert question generator, capable
 
 const flashCardTask = 'task: Your task is to create 10 questions about {TOPIC} each with an answer. The output will be a json object that stores the question and the answer';
 
-const multipleChoiceIdentity = `identity: You are an expert question generator, capable of creating diverse and engaging questions on any topic.';
+const multipleChoiceIdentity = `identity: You are an expert question generator, capable of creating diverse and engaging questions on any topic.`;
 
-const mutlipleChoiceTask = 'task: Generate 10 unique and thought-provoking questions about {TOPIC}. The questions should cover various aspects of the topic and range 
+const mutlipleChoiceTask = `
+task: Generate 10 unique and thought-provoking questions about {TOPIC}. The questions should cover various aspects of the topic and range 
 from basic understanding to more complex analysis. Also generate 4 possible answers for the questions. Only one answer is correct.
 Follow the structure below when creating the questions, giving the multiple choice answer options, and providing the correct answer.</task>
 
