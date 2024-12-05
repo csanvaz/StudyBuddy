0e38d8435b0e00fbaa2f42df2b4c82502a514533
Seth Strzechowski <sstrz2@uic.edu>
2024-12-05 14:25:53 -0600

    got rid of some warnings

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './styles/App.css';
-import { BrowserRouter as Router, Route, Routes, Navigate, useNavigate } from 'react-router-dom';
+import {Route, Routes, Navigate, useNavigate } from 'react-router-dom';
 import Login from './Login';
 import Register from './Register';
 import multiavatar from '@multiavatar/multiavatar';
