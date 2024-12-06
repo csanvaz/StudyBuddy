c0c9cad344be8c842b049692e473ff4b9e9bb378
Seth Strzechowski <sstrz2@uic.edu>
2024-12-06 08:59:04 -0600

    config for cloud v2

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
-const backendURL = "http://localhost:8080";
-//const backendURL = "https://CS484FinalProjectEnvironment-env.eba-qkbmea2x.us-east-1.elasticbeanstalk.com"
+//const backendURL = "http://localhost:8080";
+const backendURL = "https://CS484FinalProjectEnvironment-env.eba-qkbmea2x.us-east-1.elasticbeanstalk.com"
 
 export default backendURL;
